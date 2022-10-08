85137ee81557758140636965d8ecac25e4525f96
tarana <beshirova@hotmail.com>
2022-10-08 19:14:54 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Пользователь вводит с клавиатуры число M, потом вводит M чисел ЧЕРЕЗ 
+ENTER. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+Console.Clear();
+Console.Write("Введите число: ");
+int length = int.Parse(Console.ReadLine());
+Console.Write($"Введите {length} чисел от -100 до 100 через пробел, затем нажмите Enter: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
+
